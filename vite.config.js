aef2eb6f273078646ae3a8b68740671d607310fe
Joel Kasisi <joelkasisi10@gmail.com>
2024-05-02 15:44:58 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-router-dom"],
+      external: ["react-router-dom", "react-router"],
     },
   },
 });
